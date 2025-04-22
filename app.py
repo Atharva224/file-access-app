e1df866d2ba6bdc4fc3eb222cb0a6a321d90c958
Atharva224 <atharvasinnarkar@gmail.com>
2025-04-22 14:25:06 +0200

    Fix: use 0.0.0.0 and Render's PORT

--- app.py
+++ app.py
@@ -70,5 +70,9 @@ def downloads():
 def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
